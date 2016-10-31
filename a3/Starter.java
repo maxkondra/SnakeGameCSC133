341bb9f0851f7d0db5c69e7d472d1356aa3c246b
konopoly <kman239@gmail.com>
2016-10-30 18:06:18 -0700

    code cleanup

--- a3/Starter.java
+++ a3/Starter.java
@@ -6,7 +6,7 @@ import a3.Controllers.Game;
  * Created by Max on 10/5/2014.
  */
 public class Starter {
-
+	 //main method of the application
     public static void main(String[] args){
         Game game = new Game();
     }
